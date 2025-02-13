6e76791efb69423aabdf8c86c655357a2bd5d8cf
Iqbql Hossain Chapal <chapal2004@gmail.com>
2025-02-13 23:25:44 +0900

    Second day

---
+++ lib/data.ts
@@ -0,0 +1,37 @@
+const data = {
+    headeerMenus: [
+        {
+            name: "Today's Deal",
+            herf: '/search?tag=tadays-deals',
+        },
+        {
+            name: "New Arrivals",
+            herf: '/search?tag=new-arrival',
+        },
+        {
+            name: "Feature Products'",
+            herf: '/search?tag=featured',
+        },
+        {
+            name: "Best Sellers ",
+            herf: '/search?tag=best-sellar',
+        },
+        {
+            name: "Browsing History",
+            herf: '/#browsing-history',
+        },
+        {
+            name: "Customer Service",
+            herf: '/page/customer-service',
+        },
+        {
+            name: "About Us",
+            herf: '/page/about-us',
+        },
+        {
+            name: "help",
+            herf: '/page/help',
+        },
+    ],
+}
+ export default data

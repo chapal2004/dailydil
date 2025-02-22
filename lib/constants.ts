f9ab02864e100ddbcb9b7a5421d313b7f5a36200
Iqbql Hossain Chapal <chapal2004@gmail.com>
2025-02-22 11:41:00 +0900

    view page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME  || 'DailyDil'
 export const NEXT_PUBLIC_APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spnd less, enjoy more.'
 export const NEXT_PUBLIC_APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An dil clone built with Next.js andMongoDB'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
